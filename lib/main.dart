ce7eb30ed4b64d0701cd7dd86c2d3eac59eb45e8
Ali Alhashimi <libmacali@gmail.com>
2022-01-12 00:47:26 +0400

    Added Darktheme support with provider

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,68 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:snap_for_me/about.dart';
 import 'package:snap_for_me/ripple_animation.dart';
+import 'package:snap_for_me/themes/dark_theme_provider.dart';
+import 'package:snap_for_me/themes/dark_theme_style.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class ThemeClass {
+  static ThemeData lightTheme = ThemeData(
+      scaffoldBackgroundColor: Colors.white,
+      colorScheme: ColorScheme.light(),
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.blue,
+      ));
+
+  static ThemeData darkTheme = ThemeData(
+      scaffoldBackgroundColor: Colors.black,
+      colorScheme: ColorScheme.dark(),
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.black,
+      ));
+}
+
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DarkThemeProvider themeChangeProvider = DarkThemeProvider();
+
+  @override
+  void initState() {
+    super.initState();
+    getCurrentAppTheme();
+  }
+
+  void getCurrentAppTheme() async {
+    themeChangeProvider.darkTheme =
+        await themeChangeProvider.darkThemePreference.getTheme();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      home: const MyHomePage(title: 'طقطق لي'),
-      initialRoute: '/',
-      routes: {
-        '/about': (context) => const AboutMe(),
+    return ChangeNotifierProvider(
+      create: (_) {
+        return themeChangeProvider;
       },
+      child: Consumer<DarkThemeProvider>(
+        builder: (context, value, child) => MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: Styles.themeData(themeChangeProvider.darkTheme, context),
+            home: MyHomePage(title: 'طقطق لي'),
+            routes: <String, WidgetBuilder>{
+              '/Agenda': (context) => AboutMe(),
+            }),
+      ),
     );
   }
 }
@@ -39,14 +80,22 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
+    final themeChange = Provider.of<DarkThemeProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text("طقطق لي")),
         actions: [
           TextButton(
             child: Text('About'),
-            onPressed: () => Navigator.pushNamed(context, '/about'),
+            onPressed: () => Navigator.push(
+                context, MaterialPageRoute(builder: (context) => AboutMe())),
           ),
+          Switch(
+              value: themeChange.darkTheme,
+              onChanged: (bool value) {
+                themeChange.darkTheme = value;
+              })
         ],
       ),
       body: SafeArea(
